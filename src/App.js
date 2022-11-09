a40c2b233fa76d542ace856db97ae21ee6c20fa6
Sumit Rajput <sumit@fundamento.ai>
2022-11-09 18:30:20 +0530

    completed development for the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from "react";
-// apikey a21b179c
-// api: http://www.omdbapi.com/?i=tt3896198&apikey=a21b179c
+import React, { useState } from "react";
+import "./App.css";
+import search from "./assets/search.svg";
+import MovieCard from "./MovieCard";
 
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=a21b179c";
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (searchTerm) => {
+    const response = await fetch(API_URL + "&s=" + searchTerm);
+    const responseJson = await response.json();
+
+    setMovies(responseJson.Search);
+  };
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <>
-      <h1 style={{ fontFamily: "georgia" }}>Hello Potty</h1>
-    </>
+    <div className="app">
+      <h1>movie movie movie</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
+
+        <img
+          src={search}
+          alt="simple search icon"
+          onClick={() => {
+            searchTerm && searchMovies(searchTerm);
+          }}
+        />
+      </div>
+
+      <div className="container">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} />
+        ))}
+      </div>
+    </div>
   );
 };
 
